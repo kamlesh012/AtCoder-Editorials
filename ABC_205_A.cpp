3bd7fd32fb0419003a7080f54bae36e1d8cb068f
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2021-07-27 13:25:49 +0530

    Create ABC_205_A.cpp

---
+++ ABC_205_A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+#define rep(i,n,s) for(int i=0;i<n;i+=s)
+using namespace std;
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	float x = ((a * 1.00) / 100);
+
+	float z = ((b * 1.00) * x);
+ 
+	cout << z << endl;
+ 
+}
+ 
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+ 
+	solve();
+ 
+}
