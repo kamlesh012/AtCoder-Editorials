4ee76ad06c48063e67f45bfcd5ad3469932925bb
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2021-07-27 13:30:26 +0530

    Create ABC_208_A.cpp

---
+++ ABC_208_A.cpp
@@ -0,0 +1,25 @@
+//AT-COder BEginner Contest 
+#include <bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+#define rep(i,n,s) for(int i=0;i<n;i+=s)
+#define mxsize 100000
+using namespace std;
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	if (((a * 6) >= b ) && (b >= a))cout << "Yes" << endl;
+	else cout << "No" << endl;
+ 
+}
+ 
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+ 
+	solve();
+	// solve2();
+ 
+}
