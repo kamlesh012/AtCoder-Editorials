73a957278e0904403d3e53908f3bb837ebeb9796
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2021-07-27 13:13:31 +0530

    Create ABC_202_A.cpp

---
+++ ABC_202_A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+#define rep(i,n,s) for(int i=0;i<n;i+=s)
+using namespace std;
+void solve()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << (7 - a) + (7 - b) + (7 - c) << endl;
+ 
+}
+ 
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	solve();
+}
